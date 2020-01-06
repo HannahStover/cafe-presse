09f35abbde0bb6bad400d7c99ad48da8ec3313a0
Hannah Stover <hrosestover@gmail.com>
2020-01-06 13:10:38 -0800

    Update serialize-javascript dependency

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,72 +1,83 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isNavOpen: false
-        };
-        this.toggleNav = this.toggleNav.bind(this);
-    }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
-
-    render() {
-        return (
-            <>
-                <Navbar dark expand="md">
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/">
-                            <img src="assets/images/Cafe-Presse-logo-2.png" height="30" width="41"
-                                alt="Cafe Presse" />
-                        </NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        <span className=""></span> Home
-                                </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/about">
-                                        <span className=""></span> About
-                                </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/menu">
-                                        <span className=""></span> Menu
-                                </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/contactus">
-                                        <span className=""></span> Contact
-                                </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
-                </Navbar>
-                <Jumbotron>
-                    <div className="container">
-                        <div className="row row-header">
-                            <div className="col-12 col-sm-6">
-                                <h1>Cafe Presse</h1>
-                                <p>Open Daily from 8am - 1am</p>
-                            </div>
-                        </div>
-                    </div>
-                </Jumbotron>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Navbar color="light" light expand="md">
+          <div className="container">
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="assets/images/Cafe-Presse-logo-2.png"
+                height="30"
+                width="41"
+                alt="Cafe Presse"
+              />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <span className=""></span> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/about">
+                    <span className=""></span> About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/menu">
+                    <span className=""></span> Menu
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <span className=""></span> Contact
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Cafe Presse</h1>
+                <p>Open Daily from 8am - 1am</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </>
+    );
+  }
 }
 
 export default Header;
